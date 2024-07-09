c7aaf0381ea54c159c39de590a190f7bf9890b78
DhananjayGavnang <dhananjay.gavnang@gmail.com>
2024-07-09 18:19:49 +0530

    Update DupElementUsingHashMap.java

--- DupElementUsingHashMap.java
+++ DupElementUsingHashMap.java
@@ -8,6 +8,15 @@ public class Main {
     public static void main(String[] args) {
         Integer[] numbers = {1, 2, 3, 4, 5, 1, 6, 2, 7, 3, 8};
 
+        //////////////////Using hashset
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> duplicates = Arrays.stream(numbers)
+            .filter(n -> !set.add(n))
+            .collect(Collectors.toSet());
+
+        System.out.println("Duplicate numbers: " + duplicates);
+
+        //////////////////////Using HashMap
         Map<Integer, Long> numberCounts = Arrays.stream(numbers)
                 .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
 
